b378da2675caffe6bf881dbc18eccd514ea5ff91
WarrenWu4 <warrenweiwu04@gmail.com>
2025-03-13 15:42:52 -0500

    added custom session callback with the user id

--- slamfunk-website/app/utils/authConfig.ts
+++ slamfunk-website/app/utils/authConfig.ts
@@ -30,6 +30,12 @@ export const authConfig = {
             }
             return false;
         },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.sub as string;
+            }
+            return session;
+        },
     },
     pages: {
         signIn: '/login',
