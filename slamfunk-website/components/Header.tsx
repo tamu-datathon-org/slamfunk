5b7d8db6ab3bd02573caee40c69b2eb4d8d5871c
Rishab Jadhav <rishabjadhav@Rishabs-Air.attlocal.net>
2025-03-14 00:22:51 -0500

    viltrumite dna

--- slamfunk-website/components/Header.tsx
+++ slamfunk-website/components/Header.tsx
@@ -44,14 +44,14 @@ export default function Header() {
                 Challenge
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link
                 href="/bracket"
                 className="text-sm text-gray-800 dark:text-white px-4 py-2 rounded-md hover:bg-blue-50 dark:hover:bg-blue-800 transition-colors"
               >
                 Bracket
               </Link>
-            </li>
+            </li> */}
             <li>
               <a
                 href="#faq-section"
